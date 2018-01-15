e3fdf60867d68a4694c03574d5b496f61683e915
sentient64 <m.alsharekh94@gmail.com>
2018-01-15 10:43:37 +0300

    updated database credentials in settings.py

--- project/settings.py
+++ project/settings.py
@@ -95,9 +95,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'nietzsche',                      # Or path to database file if using sqlite3.
+            'NAME': 'why',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': 'postgres',
+            'USER': 'friedrich',
             'PASSWORD': 'nietzsche',
             'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '',                      # Set to empty string for default.
